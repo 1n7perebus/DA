f7daa5616d185ff52cb526cbe4793f02b5eded03
unknown <areshe90@gmail.com>
2024-04-23 06:10:56 -0700

    update all

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
+from django.views.static import serve
+from django.conf import settings
 
 app_name = "main"
 
@@ -7,4 +9,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path('dreams/', views.dreams, name='dreams'),
     path('dreams/<uuid:dream_id>/', views.dreams, name='specific_dream'),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
